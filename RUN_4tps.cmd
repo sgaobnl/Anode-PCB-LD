4ef85934647d2e9cd0fec0ad85c2e52e14f8248f
Shanshan Gao <gao33.bnl@gmail.com>
2020-03-31 15:36:17 -0400

    Update RUN_4tps.cmd

--- RUN_4tps.cmd
+++ RUN_4tps.cmd
@@ -1,5 +1,6 @@
 python "./MainCOTS.py" SBND_TOP 0x00 True 0
-python "./MainCOTS.py" SBND_TOP 0x13 True 0
+"python "./MainCOTS.py" SBND_TOP 0x13 True 0
+python "./MainCOTS.py" SBND_TOP 0x20 True 0
 python "./MainCOTS.py" SBND_TOP 0x80 True 0
 python "./APA_analysis.py" 
 python "./apa_plot_main.py" 
