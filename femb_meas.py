f9949def84e380649fb312b9f8cca53ddce783f3
Shanshan Gao <gao33.bnl@gmail.com>
2020-04-02 01:53:39 -0400

    update

--- femb_meas.py
+++ femb_meas.py
@@ -663,7 +663,7 @@ class FEMB_MEAS: #for one FEMB
 
         for femb_addr in fembs_np:
             self.fe_reg.set_fe_board() # reset the registers value
-            self.fe_reg.set_fe_board(sg=sg, st=tp, sts=1, smn=0, sdf=1, slk0=slk0, slk1=slk1, swdac =2, dac=0 )
+            self.fe_reg.set_fe_board(sg=sg, st=tp, sts=0, smn=0, sdf=1, slk0=slk0, slk1=slk1, swdac =2, dac=0 )
             self.fe_reg.set_fechn_reg(chip=0, chn=0, sts=1, sg=1, st=0)
             self.fe_reg.set_fechn_reg(chip=1, chn=1, sts=1, sg=1, st=1)
             self.fe_reg.set_fechn_reg(chip=2, chn=2, sts=1, sg=1, st=2)
--- trig_raw_convertor.py
+++ trig_raw_convertor.py
@@ -23,7 +23,8 @@ from sys import exit
 import os.path
 import copy
 
-def rawto32chn(onepkgdata, i, chn_data):
+def rawto32chn(onepkgdata, chn_data):
+    i = 0
     if (onepkgdata[i] == 0xface ) or (onepkgdata[i] == 0xfeed ):
         if (onepkgdata[i] == 0xface ):
             pre = 0x00000
@@ -75,49 +76,90 @@ def rawto32chn(onepkgdata, i, chn_data):
         print ("ERROR")
         cycle_del = True
     i = i + 25 
-    return chn_data, i ,cycle_del
+    return chn_data ,cycle_del
 
 def raw_convertor_conv(fp, jumbo_flag=True):
     if (jumbo_flag == True):
-        pkg_len = int(0x1E06/2)
+        pkg_len = int(0x1df4/2)
     else:
-        pkg_len = int(0x406/2)
-    chn_data = []
-    for i in range(32):
-        chn_data.append([])
+        pkg_len = int(0x3fa/2)
     with open(fp, 'rb') as f:
         raw_data = f.read()                
         len_file = len(raw_data) 
         dataNtuple =struct.unpack_from(">%dH"%(len_file//2),raw_data)
 
-        for addr in range(0, len(dataNtuple) ):
+        addr = 0
+        links_face_pos = [np.array([]), np.array([]), np.array([]), np.array([])]
+        links_feed_pos = [np.array([]), np.array([]), np.array([]), np.array([])]
+        link_i = 0
+        while (addr <= len(dataNtuple) - 25 ):
             pkg_cnt0 = ((((dataNtuple[addr]) << 16 )+ (dataNtuple[addr+1])) ) & 0xFFFFFFFF
+            pkg_res0 = ((((dataNtuple[addr+2]) << 16 )+ (dataNtuple[addr+3])) ) & 0xFFFFFFFF
+            a = addr
+            for b in range(addr, len(dataNtuple)-4, 1):
+                #print (b)
+                pkg_cnt1 = ((((dataNtuple[b]) << 16 )+ (dataNtuple[b+1])) ) & 0xFFFFFFFF
+                pkg_res1 = ((((dataNtuple[b+2]) << 16 )+ (dataNtuple[b+3])) ) & 0xFFFFFFFF
+                if (pkg_cnt1 == pkg_cnt0 + 1) and (pkg_res0 == 0) and (pkg_res1 == 0):
+                    break 
+            udp_pkg = np.array(dataNtuple[a:b])
+            face_pos = np.where(udp_pkg == 0xface)[0]
+            feed_pos = np.where(udp_pkg == 0xfeed)[0]  
+            face_pos = np.sort( (np.append(face_pos, feed_pos))) + a
+            feed_pos =  feed_pos + a
+            addr = b
+            links_face_pos[link_i] = np.append(links_face_pos[link_i] , face_pos)
+            links_feed_pos[link_i] = np.append(links_feed_pos[link_i] , feed_pos)
+            if (b-a) < pkg_len:
+                link_i +=1
+        link_data = [[],[],[],[]]
+        for i in range(len(links_face_pos)):
+            chn_data = []
+            for x in range(32):
+                chn_data.append([])
+            for j in range(len(links_face_pos[i])-1):
+                if (links_face_pos[i][j+1] - links_face_pos[i][j] == 25): 
+                    onepkgdata = dataNtuple[int(links_face_pos[i][j]) : int(links_face_pos[i][j]) +25 ]
+                    chn_data, cycle_del =  rawto32chn(onepkgdata, chn_data)
+            link_data[i] = chn_data
+        femb_data = link_data[0] + link_data[0] + link_data[0] + link_data[0] 
+#        for i in range(128):
+#            print (len(femb_data[i]))
+    return femb_data
 
-        for addr in range(0,len(dataNtuple) - pkg_len,pkg_len):
-            pkg_cnt0 = ((((dataNtuple[addr]) << 16 )+ (dataNtuple[addr+1])) + 1 ) & 0xFFFFFFFF
-            pkg_cnt1 = (((dataNtuple[addr+pkg_len]) << 16 )+ (dataNtuple[addr+1+pkg_len])) &0xFFFFFFFF
-            print (hex(addr*2+0x1e06), hex(pkg_cnt0), hex(pkg_cnt1))
-            if (pkg_cnt0 != pkg_cnt1):
-                print ("%s: UDP package size is not right! "%(fp))
-                cycle_del = True
-                break
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+import matplotlib.mlab as mlab
 
-            onepkgdata = dataNtuple[addr : addr + pkg_len]
 
-            for i in range(8,pkg_len,1):
-                if (onepkgdata[i] == 0xface ) or (onepkgdata[i] == 0xfeed ):
-                    break
+def mk_plot(femb_data):
+    for j in range(8):
+        fig = plt.figure(figsize=(16,8))
+        plt.rcParams.update({'font.size': 6})
+        for i in range(16):
+            chn = 16*j + i
+#            print ((int(i//2), int(i%8)))
+            ax = plt.subplot2grid((8,2), (int(i%8), int(i//8)),  colspan=1, rowspan=1)
+            x = np.arange(len(femb_data[chn])) * 0.5
+            y = np.array(femb_data[chn])%0x10000
+            ax.plot(x[0:150], y[0:150], marker = '.', label="CH%d"%chn)
+            ax.set_ylim((1000, 3500))
+            ax.set_xlabel("Time / us")
+            ax.set_ylabel("ADC counts / LSB")
+            ax.legend(loc = 1)
+            ax.grid()
 
-            while i < (len(onepkgdata)-25) :
-                chn_data, i ,cycle_del =  rawto32chn(onepkgdata, i, chn_data)
-        for smp in range(len(chn_data[0])):
-            if ((chn_data[0][smp] & 0x10000) == 0x10000): # feed align
-                break
- 
-    return chipdata
+        plt.tight_layout()
+        plt.show()
+  #      plt.savefig(save_fp)
+        plt.close()
 
-fp = "/Users/shanshangao/Documents/tmp/run08tri/WIB00step18_FEMB0_B9_158578232573.bin"
-chipdata = raw_convertor_conv(fp)
+
+
+fp = "/Users/shanshangao/Documents/tmp/run11tri/WIB00step18_FEMB0_B8_158580569657.bin"
+fembdata = raw_convertor_conv(fp)
+mk_plot(fembdata)
 #def femb_raw(wib_cycle_femb_chip, femb=0, sync_chns=128):
 #    femb_rawdata = []
 #    for chn in range(sync_chns):
