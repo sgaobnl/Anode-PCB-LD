1bb04182097404b7f8b7f8ae05a79531af361df9
Shanshan Gao <gao33.bnl@gmail.com>
2020-04-02 21:28:03 -0400

    update

---
+++ RUN_external_trigger.cmd
@@ -0,0 +1,5 @@
+python "./MainCOTS.py"  Anode_PCB 0x00 True 0
+python "./MainCOTS.py"  Anode_PCB 0x08 True 0
+python "./MainCOTS.py"  Anode_PCB 0x80 True 0
+PAUSE
+
--- RUN_software_trigger.cmd
+++ RUN_software_trigger.cmd
@@ -1,8 +1,5 @@
 python "./MainCOTS.py"  Anode_PCB 0x00 True 0
-"python "./MainCOTS.py" Anode_PCB 0x13 True 0
 python "./MainCOTS.py"  Anode_PCB 0x20 True 0
 python "./MainCOTS.py"  Anode_PCB 0x80 True 0
-python "./APA_analysis.py" 
-python "./apa_plot_main.py" 
 PAUSE
 
